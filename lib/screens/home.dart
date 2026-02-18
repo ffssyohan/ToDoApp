24b0c6490f1606c914054bbfb3d338383a73057b
Felipe Yohan Sapio <ffssyohan@gmail.com>
2026-02-18 20:31:38 -0300

    Created a custom AppBar and Search Bar and implemented on the Home Screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,10 +7,55 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ToDo App')),
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
       body: Container(
-        child: Text('home screen'),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(children: [searchBox()]),
       ),
+    );
+  }
+
+  //TODO refactor it into widgets
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(Icons.menu, color: tdBlack, size: 30),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(20),
+              child: Image.asset('assets/images/ozy.jpg'),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: tdBGColor,
     );
   }
 }
